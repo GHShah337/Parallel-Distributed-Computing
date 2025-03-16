c2f6f198a81da501492ecc47303de821eb87cd01
GHShah337 <60106493@udst.edu.qa>
2025-03-16 12:21:00 +0300

    did celery

---
+++ src/first_celery.py
@@ -0,0 +1,8 @@
+from src.tasks import power
+
+def dispatch_tasks():
+    result_objs = [power.apply_async((number, 2)) for number in range(1, 10001)]
+    results = [result.get() for result in result_objs]
+    return results
+
+
